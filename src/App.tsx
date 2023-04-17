aa7e7c626c68363f4515649e94cbb5267411feb4
Rodrigo Polydoro <rspolydoro@gmail.com>
2023-04-17 11:01:34 -0300

    Added theme feat()

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import {AppRoutes} from "./routes/AppRoutes";
+import { ThemeProvider } from '@material-ui/core/styles';
+import {LightTheme} from "./shared/themes";
+
 const App: React.FC = () => {
     return (
-        <BrowserRouter>
-            <AppRoutes/>
-        <div>
-            {/*<Hello name="SPFC" />*/}
-        </div>
-        </BrowserRouter>
+        <ThemeProvider theme={LightTheme}>
+            <BrowserRouter>
+                <AppRoutes/>
+                <div>
+                    {/*<Hello name="SPFC" />*/}
+                </div>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 export default App
